d27dd0134ac560c8e861a06748b7b65ecd46890a
Manuel Eusebio de Paz Carmona <Pelirrojo@users.noreply.github.com>
2023-04-04 14:30:25 +0000

    TF based on EKS blueprint example argoCD

---
+++ iac/locals.tf
@@ -0,0 +1,20 @@
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
+
+data "aws_availability_zones" "available" {}
+
+locals {
+  name   = basename(path.cwd)
+  region = "eu-west-1"
+
+  cluster_version = "1.24"
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  tags = {
+    Blueprint  = local.name
+    GithubRepo = "github.com/aws-ia/terraform-aws-eks-blueprints"
+  }
+}
