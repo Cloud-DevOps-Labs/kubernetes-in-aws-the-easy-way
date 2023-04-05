d19c9fc80be023115855a4de3ad349c63554f409
Manuel Eusebio de Paz Carmona <Pelirrojo@users.noreply.github.com>
2023-04-05 14:24:25 +0000

    bootstrap-argocd

--- IaC/main.tf
+++ IaC/main.tf
@@ -66,7 +66,7 @@ module "vpc" {
 }
 
 #
-# Blueprints
+# Blueprint Setup
 ################################################################################
 
 module "eks_blueprints" {
@@ -138,4 +138,47 @@ module "eks_blueprints" {
   }
 
   tags = local.tags
+}
+
+#
+# Kubernetes addons
+################################################################################
+
+module "kubernetes_addons" {
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.21.0/modules/kubernetes-addons"
+
+  eks_cluster_id     = module.eks_blueprints.eks_cluster_id
+
+  #---------------------------------------------------------------
+  # ARGO CD ADD-ON
+  #---------------------------------------------------------------
+
+  enable_argocd         = true
+  argocd_manage_add_ons = true # Indicates that ArgoCD is responsible for managing/deploying Add-ons.
+
+  argocd_applications = {
+    addons    = local.addon_application
+    #workloads = local.workload_application #We comment it for now
+  }
+
+  argocd_helm_config = {
+    set = [
+      {
+        name  = "server.service.type"
+        value = "LoadBalancer"
+      }
+    ]
+  }
+
+  #---------------------------------------------------------------
+  # ADD-ONS - You can add additional addons here
+  # https://aws-ia.github.io/terraform-aws-eks-blueprints/add-ons/
+  #---------------------------------------------------------------
+
+
+  enable_aws_load_balancer_controller  = true
+  enable_amazon_eks_aws_ebs_csi_driver = true
+  enable_aws_for_fluentbit             = true
+  enable_metrics_server                = true
+
 }
