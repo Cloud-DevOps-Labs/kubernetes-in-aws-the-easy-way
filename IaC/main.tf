744914f60636a18b62ba382d9da8fcbbb0a5c6b0
Manuel Eusebio de Paz Carmona <Pelirrojo@users.noreply.github.com>
2023-04-05 12:42:19 +0000

    Provision cluster

--- IaC/main.tf
+++ IaC/main.tf
@@ -1,3 +1,33 @@
+#
+# Providers
+################################################################################
+
+provider "kubernetes" {
+  host                   = module.eks_blueprints.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks_blueprints.eks_cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
+}
+
+provider "kubectl" {
+  apply_retry_count      = 10
+  host                   = module.eks_blueprints.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
+  load_config_file       = false
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+#
+# Networking
+################################################################################
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.16.0"
@@ -33,4 +63,42 @@ module "vpc" {
   }
 
     tags = local.tags
+}
+
+#
+# Blueprints
+################################################################################
+
+module "eks_blueprints" {
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.21.0"
+
+  cluster_name    = local.name
+
+  # EKS Cluster VPC and Subnet mandatory config
+  vpc_id             = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnets
+
+  # EKS CONTROL PLANE VARIABLES
+  cluster_version = local.cluster_version
+
+  # List of Additional roles admin in the cluster
+  # Comment this section if you ARE NOT at an AWS Event, as the TeamRole won't exist on your site, or replace with any valid role you want
+  #map_roles = [
+  #  {
+  #    rolearn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/TeamRole"
+  #    username = "ops-role" # The user name within Kubernetes to map to the IAM role
+  #    groups   = ["system:masters"] # A list of groups within Kubernetes to which the role is mapped; Checkout K8s Role and Rolebindings
+  #  }
+  #]
+
+  # EKS MANAGED NODE GROUPS
+  managed_node_groups = {
+    mg_5 = {
+      node_group_name = local.node_group_name
+      instance_types  = ["m5.xlarge"]
+      subnet_ids      = module.vpc.private_subnets
+    }
+  }
+
+  tags = local.tags
 }
