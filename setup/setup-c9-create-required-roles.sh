bcad1a53204deeadbbf69d4af87cd7ce962dbfbf
Manuel Eusebio de Paz Carmona <Pelirrojo@users.noreply.github.com>
2023-04-10 13:43:24 +0000

    Improve scripts

--- setup/setup-c9-create-required-roles.sh
+++ setup/setup-c9-create-required-roles.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 aws --version >/dev/null 2>&1 || { echo >&2 "I require AWS CLI but it's not installed. ¯\_(ツ)_/¯ Aborting."; exit 1; }
 
+# Create a role for the C9 environment
 ROLENAME=eks-blueprints-for-terraform-workshop-admin
 
 aws iam create-role \
@@ -11,4 +12,5 @@ aws iam attach-role-policy \
     --role-name $ROLENAME
     
 
-
+# Create a service role for spot instances
+aws iam create-service-linked-role --aws-service-name spot.amazonaws.com
